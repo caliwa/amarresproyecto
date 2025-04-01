8d71ee06d744771fa0f550be9cac30ff4814a65a
DoblamosSA <carlos_gonzalez82201@elpoli.edu.co>
2025-03-31 20:31:34 -0500

    test storage private

--- app/Livewire/Menu/IndexMenuComponent.php
+++ app/Livewire/Menu/IndexMenuComponent.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Storage;
 class IndexMenuComponent extends Component
 {
     public function mount(){
-        dd(Storage::disk('private'));
+        // dd(Storage::disk('private'));
     }
 
     public function render()
