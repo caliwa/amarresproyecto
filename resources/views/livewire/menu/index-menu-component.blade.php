f78b1f4541976ccf351d3b341be7b16f22c3378a
DoblamosSA <carlos_gonzalez82201@elpoli.edu.co>
2025-04-04 09:31:06 -0500

    advice to div

--- resources/views/livewire/menu/index-menu-component.blade.php
+++ resources/views/livewire/menu/index-menu-component.blade.php
@@ -825,7 +825,7 @@ document.addEventListener('livewire:initialized', () => {
                         <svg class="w-8 h-8 text-white animate-pulse" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M12 9V14M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21ZM12 17H12.01" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
                         </svg>
-                        <h2 class="text-4xl font-black text-white tracking-wider">ADVERTENCIA</h2>
+                        <div class="text-4xl font-black text-white tracking-wider">ADVERTENCIA</div>
                         <svg class="w-8 h-8 text-white animate-pulse" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M12 9V14M12 21C16.9706 21 21 16.9706 21 12C21 7.02944 16.9706 3 12 3C7.02944 3 3 7.02944 3 12C3 16.9706 7.02944 21 12 21ZM12 17H12.01" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
                         </svg>
