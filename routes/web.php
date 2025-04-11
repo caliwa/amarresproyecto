a71a1de752e6f3b72849c11c003a17f02256f67f
DoblamosSA <carlos_gonzalez82201@elpoli.edu.co>
2025-04-10 23:06:36 -0500

    web php routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', MediatorMenuComponent::class)->name('menu');
+Route::get('/nosotros', MediatorMenuComponent::class)->name('menu');
+Route::get('/servicios', MediatorMenuComponent::class)->name('menu');
+Route::get('/contactanos', MediatorMenuComponent::class)->name('menu');
 
 // Route::get('/landing', MediatorDashboardComponent::class)->name('dashboard');
 
